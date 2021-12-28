042d7902978282433d5a21344c80d57a104c6f29
ATSmusz <65310062+asmusz-adobe@users.noreply.github.com>
2021-12-28 14:13:06 -0800

    Update upload-transient-file.ps1

--- upload-transient-file.ps1
+++ upload-transient-file.ps1
@@ -1,3 +1,8 @@
+# replace the authorization header with your token or integration key
+# replace email address in the x-api-user with your sender's email
+# make sure you use the correct "shard" (.na2.) with the relevant one for your account
+#        See the code to get the correct access point in this repo if you don't know what "shard" your account is on
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Content-Type", "application/x-www-form-urlencoded")
