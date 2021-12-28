2e1baebfb095f6d26e804b7c5b0b4e7a8b04aa87
ATSmusz <65310062+asmusz-adobe@users.noreply.github.com>
2021-12-27 23:54:15 -0800

    Update get-access-point.ps1

--- get-access-point.ps1
+++ get-access-point.ps1
@@ -3,10 +3,15 @@
 
 $url = "https://api.echosign.com/api/rest/v6/baseUris"
 
-$header = @{"Authorization" = "Bearer 3AAABLblqZhD7gPD****** your access token or integration key here ******IojNCU2WqAsaG1oYtllVLaHv9e";
-"x-api-user"= "email:sender-email@yourdomain.com"}
+$header = @{"Authorization" = "Bearer 3AAABLblqZhD7gPDMJ5vjNsKgHXbUxh3eGm3Y2U_iTYlh40xEKV0n3JaoDk3uPhO5VSYrheIojNCU2WqAsaG1oYtllVLaHv9e";
+"x-api-user"= "email:echosmusz1+na2main@gmail.com"}
 
 $Result = Invoke-RestMethod -Uri $url -Method Get -Headers $header
 
-Write-Host $result
-Write-Host $Result.apiAccessPoint 
+$ResultJson = $Result | ConvertTo-Json
+
+Write-Host $ResultJson
+
+Write-Host $Result
+
+Write-Host $Result.apiAccessPoint
